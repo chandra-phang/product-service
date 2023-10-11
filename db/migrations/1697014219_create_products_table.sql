15932c9b043d6d05ec27e318b2d2683e5b7f0485
Chandra Phang <chandra.2@circles.asia>
2023-10-11 22:10:16 +0700

    add quota to products table

--- db/migrations/1697014219_create_products_table.sql
+++ db/migrations/1697014219_create_products_table.sql
@@ -1,6 +1,7 @@
 CREATE TABLE "products" (
   "id" varchar(100) PRIMARY KEY,
   "name" varchar(255) NOT NULL,
+  "daily_quota" int,
   "created_at" TIMESTAMP NOT NULL,
   "updated_at" TIMESTAMP NOT NULL
 )
